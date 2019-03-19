99b230d8e9ce3bbcce98d86653944f0a79a6bfb4
Eddie Bird <eddiebird13@gmail.com>
2019-03-19 11:58:11 -0700

    Usage comment for script
    This script is only used for "calibration", or when a different Arduino is used. In the RPi server file, the serial number of the Arduino is hardcoded so that the RPi can connect to the serial port no matter which usb port it is plugged into. If a different Arduino is used, this number will change and the RPi server file needs to be updated to the new serial number.

--- usbScan.py
+++ usbScan.py
@@ -1,3 +1,5 @@
+# This file is just used for determining the serial number of an attached Arduino. 
+# It is only necessary when a new Arduino is used so that the RPi can identify it on a usb port.
 import re
 import subprocess
 device_re = re.compile("Bus\s+(?P<bus>\d+)\s+Device\s+(?P<device>\d+).+ID\s(?P<id>\w+:\w+)\s(?P<tag>.+)$", re.I)
