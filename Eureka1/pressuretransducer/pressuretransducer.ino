e5b0cf6a99d58e7074c7887f9c4ce5d479884f9e
Vamshi Balanaga <vamshi.balanaga@berkeley.edu>
2019-10-23 16:21:42 -0700

    rearranged dir

---
+++ Eureka1/pressuretransducer/pressuretransducer.ino
@@ -0,0 +1,12 @@
+// This code will run on a nano connected to either one or two pressure transducers that read the
+// pressure of the nitrogen tanks (2) or the lox (1) or the propane (1) tanks.
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
