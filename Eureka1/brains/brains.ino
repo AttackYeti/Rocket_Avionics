b92bdb9f5bb2acd3f9a8a04fb50a22b176b721aa
Vamshi Balanaga <vamshi.balanaga@berkeley.edu>
2019-10-26 07:49:21 -0700

    update brains for data collection

--- Eureka1/brains/brains.ino
+++ Eureka1/brains/brains.ino
@@ -8,17 +8,18 @@
 #define serialReceive LOW
 
 // ~~~~~~~~~~~~~~~ COMMUNICATION CODES ~~~~~~~~~~~~~~~~~~
-#define PRESSURANT 1 // for pressurant motor controller 
-#define TANKS 2 // for tanks motor controller
-#define MOTORA 3
-#define MOTORB 4
+#define PRESSURANT_MOTOR 1 // for pressurant motor controller 
+#define TANKS_MOTOR 2 // for tanks motor controller
+#define LOX_MOTOR 3
+#define PROP_MOTOR 4
 
 #define N_LOX_TRANSDUCER 11 //high
 #define N_PROP_TRANSDUCER 12 //high
 #define LOX_TRANSDUCER 13 //low
 #define PROP_TRANSDUCER 14 //low
 
-
+int motor_lox_pressurant_pos = 0;
+int motor_prop_pressurant_pos = 0;
 
 int n_lox_pressure = 0;
 int n_prop_pressure = 0;
@@ -30,7 +31,14 @@ SoftwareSerial serial(serialRX, serialTX);
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(serialControl, OUTPUT);
+  digitalWrite(serialControl, serialReceive);
 
+  serial.begin(9600);
+  Serial.begin(9600);
+  
+  Serial.println("time, pressure, pos");
+  print_state();
 }
 
 void loop() {
@@ -44,35 +52,55 @@ void loop() {
   // calculate appropriate control for desired flow rate
 
   // apply control
+  //~~~~~~~~~CHANGE THIS FOR DATA COLLECTION - NEED DIFFERENT RATES, EVEN CONSTANT POS ~~~~~~~~~~~~~~~`
+  motor_lox_pressurant_pos = adjust_motor(PRESSURANT_MOTOR, LOX_MOTOR, 1)
 
   // transmit and store state
+  print_state();
   // @SQ networking stuff
   // special board for sd storage
 
+
+}
+
+void print_state(){
+  Serial.print(micros());
+  Serial.print(",");
+  Serial.print(n_lox_pressure);
+  Serial.print(",");
+  Serial.println(motor_lox_pressurant_pos);
 }
 
 int desired_choke_area_given_pressure_differential(int curr_p_tank, int curr_p_n2){
   
 }
 
-bool adjust_motor(int board_id, int motor_id, int amount){
+int adjust_motor(int board_id, int motor_id, int amount){
   send_message([board_id, amount, motor_id]);
+  return serial.read();
 }
 
+void send_message(int[] data){
+  digitalWrite(serialControl, serialTransmit);
+  serial.write(data[0]);
+  serial.write(data[1]);
+  serial.write(data[2]);
+  digitalWrite(serialControl, serialReceive);
+}
 
 
 void read_all_pressure_values(){
   n_lox_pressure = read_pressure(N_LOX_TRANSDUCER);
   n_lox_pressure = high_pressure_sensor_conversion(n_lox_pressure);
   
-  n_prop_pressure = read_pressure(N_PROP_TRANSDUCER);
-  n_prop_pressure = high_pressure_sensor_conversion(n_prop_pressure);
+  //n_prop_pressure = read_pressure(N_PROP_TRANSDUCER);
+  //n_prop_pressure = high_pressure_sensor_conversion(n_prop_pressure);
   
-  lox_pressure = read_pressure(LOX_TRANSDUCER);
-  lox_pressure = low_pressure_sensor_conversion(lox_pressure);
+  //lox_pressure = read_pressure(LOX_TRANSDUCER);
+  //lox_pressure = low_pressure_sensor_conversion(lox_pressure);
   
-  prop_pressure = read_pressure(PROP_TRANSDUCER);
-  prop_pressure = low_pressure_sensor_conversion(prop_pressure);
+  //prop_pressure = read_pressure(PROP_TRANSDUCER);
+  //prop_pressure = low_pressure_sensor_conversion(prop_pressure);
 }
 
 int _read_pressure(int sensor_num){ // modify this using general communications method.
@@ -85,13 +113,13 @@ int _read_pressure(int sensor_num){ // modify this using general communications 
 
 float low_pressure_sensor_conversion(int raw){
   // fill this out based on characterization of transducer
-  pressure = ;
+  pressure = raw;
   return pressure;
 }
 
 float high_pressure_sensor_conversion(int raw){
   // fill this out based on characterization of transducer
-  pressure = ;
+  pressure = raw;
   return pressure;
 }
 
@@ -105,68 +133,9 @@ float curr_mass_flow_rate(float curr_pressure, int tank_id){
     int motor_position = prop_pressurant_motor_position;
   }
   float CdA = cda(curr_pressure, motor_position);
-  return CdA * sqrt(BETA * pow(curr_pressure, 2));
+  return CdA * curr_pressure * sqrt(BETA);
 }
 
 float cda(float pressure, float motor_position){
   return 0;
-}
-
-table CdA_pressure = createTable(200);
-
-
-
-// ~~~~~~~~~~~~~~~~~~~HASH MAP SOURCE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-struct node{
-    int key;
-    int val;
-    struct node *next;
-};
-struct table{
-    int size;
-    struct node **list;
-};
-struct table *createTable(int size){
-    struct table *t = (struct table*)malloc(sizeof(struct table));
-    t->size = size;
-    t->list = (struct node**)malloc(sizeof(struct node*)*size);
-    int i;
-    for(i=0;i<size;i++)
-        t->list[i] = NULL;
-    return t;
-}
-int hashCode(struct table *t,int key){
-    if(key<0)
-        return -(key%t->size);
-    return key%t->size;
-}
-void insert(struct table *t,int key,int val){
-    int pos = hashCode(t,key);
-    struct node *list = t->list[pos];
-    struct node *newNode = (struct node*)malloc(sizeof(struct node));
-    struct node *temp = list;
-    while(temp){
-        if(temp->key==key){
-            temp->val = val;
-            return;
-        }
-        temp = temp->next;
-    }
-    newNode->key = key;
-    newNode->val = val;
-    newNode->next = list;
-    t->list[pos] = newNode;
-}
-int lookup(struct table *t,int key){
-    int pos = hashCode(t,key);
-    struct node *list = t->list[pos];
-    struct node *temp = list;
-    while(temp){
-        if(temp->key==key){
-            return temp->val;
-        }
-        temp = temp->next;
-    }
-    return -1;
 }
